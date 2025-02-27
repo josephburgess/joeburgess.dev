1dacc73cb53599686807adfeed5777b6c6025221
josephburgess <josephburgess@gmail.com>
2025-02-27 19:14:16 -0300

    update content; add animation

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 type PageData struct {
@@ -14,11 +17,46 @@ type PageData struct {
 	IsDarkMode   bool
 }
 
+func fileWatcher(tmplPath string, lastModTime *time.Time, tmplPtr **template.Template) {
+	for {
+		time.Sleep(1 * time.Second)
+
+		info, err := os.Stat(tmplPath)
+		if err != nil {
+			continue
+		}
+
+		if info.ModTime().After(*lastModTime) {
+			log.Println("Template changed, reloading...")
+
+			newTmpl, err := template.ParseFiles(tmplPath)
+			if err != nil {
+				log.Printf("Error parsing template: %v", err)
+				continue
+			}
+
+			*tmplPtr = newTmpl
+			*lastModTime = info.ModTime()
+			log.Println("Template reloaded successfully")
+		}
+	}
+}
+
 func main() {
-	tmpl, err := template.ParseFiles("templates/index.html")
+	tmplPath := filepath.Join("templates", "index.html")
+
+	tmpl, err := template.ParseFiles(tmplPath)
 	if err != nil {
 		log.Fatalf("Error parsing template: %v", err)
 	}
+
+	info, err := os.Stat(tmplPath)
+	if err != nil {
+		log.Fatalf("Error getting file info: %v", err)
+	}
+	lastModTime := info.ModTime()
+
+	go fileWatcher(tmplPath, &lastModTime, &tmpl)
 
 	data := PageData{
 		ProfileImage: "/static/images/profile.png",
@@ -31,12 +69,45 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	http.HandleFunc("/livereload.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		w.Write([]byte(`
+			(function() {
+				const checkForReload = () => {
+					fetch('/check-reload')
+						.then(response => response.text())
+						.then(data => {
+							if (data === 'reload') {
+								console.log('Reloading page...');
+								window.location.reload();
+							}
+						})
+						.catch(err => console.error('Error checking for reload:', err));
+				};
+
+				setInterval(checkForReload, 1000);
+				console.log('Live reload enabled');
+			})();
+		`))
+	})
+
+	lastReloadTime := time.Now()
+
+	http.HandleFunc("/check-reload", func(w http.ResponseWriter, r *http.Request) {
+		if lastModTime.After(lastReloadTime) {
+			lastReloadTime = time.Now()
+			w.Write([]byte("reload"))
+		} else {
+			w.Write([]byte("ok"))
+		}
+	})
+
 	http.HandleFunc("/toggle-theme", func(w http.ResponseWriter, r *http.Request) {
 		cookie := &http.Cookie{
 			Name:     "theme",
 			Value:    "light",
 			Path:     "/",
-			MaxAge:   86400 * 30,
+			MaxAge:   86400 * 30, // 30 days
 			HttpOnly: true,
 		}
 
@@ -65,5 +136,6 @@ func main() {
 	})
 
 	log.Println("Server starting on http://localhost:8080")
+	log.Println("Live reload enabled - changes to templates will auto-refresh the browser")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
